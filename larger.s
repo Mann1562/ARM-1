39012aa5eafb8a1c887d9d05199c2c1cc1c6c88f
mann <71485257+Mann1562@users.noreply.github.com>
2023-04-12 16:06:46 -0500

    Update larger.s

--- larger.s
+++ larger.s
@@ -1,86 +1,58 @@
-    .global main
-    .text
+        .text
+        .global main
+
 main:
-    // allocate space for input1 and input2
-    sub sp, sp, #8
-    
-    // read in input1
-    adr x0, input1    // load address of input1 into x0
-    bl read_int       // read integer from input1
-    strb w0, [sp, #4] // store integer in input1
-    
-    // read in input2
-    adr x0, input2    // load address of input2 into x0
-    bl read_int       // read integer from input2
-    strb w0, [sp]     // store integer in input2
-    
-    // compare input1 and input2
-    ldrb w0, [sp, #4] // load integer from input1
-    ldrb w1, [sp]     // load integer from input2
-    cmp w0, w1        // compare input1 and input2
-    bge print_input1  // if input1 >= input2, print input1
-    b print_input2    // else print input2
-    
-print_input1:
-    // print input1
-    adr x0, input1_msg // load address of input1_msg into x0
-    bl print_string     // print input1_msg
-    ldrb w0, [sp, #4]   // load integer from input1
-    bl print_int        // print integer
-    b exit_program      // exit program
-    
-print_input2:
-    // print input2
-    adr x0, input2_msg // load address of input2_msg into x0
-    bl print_string     // print input2_msg
-    ldrb w0, [sp]       // load integer from input2
-    bl print_int        // print integer
-    b exit_program      // exit program
-    
-exit_program:
-    // free space used by input1 and input2
-    add sp, sp, #8
-    // exit program
-    mov x0, #0
-    ret
-    
-read_int:
-    // allocate space for input buffer
-    sub sp, sp, #4
-    // load address of input buffer into x1
-    mov x1, sp
-    // read input into buffer
-    mov x2, #2
-    mov x8, #0
-    svc #0
-    // convert input buffer to integer
-    ldrb w0, [sp]
-    msub w0, w0, w0, #10
-    add w0, w0, #48
-    ldrb w1, [sp, #1]
-    msub w1, w1, w1, #10
-    add w1, w1, #48
-    mul w0, w0, #10
-    add w0, w0, w1
-    // free space used by input buffer
-    add sp, sp, #4
-    ret
-    
-print_string:
-    // print string pointed to by x0
-    mov x1, #0
-    bl printf
-    ret
-    
-print_int:
-    // print integer in w0
-    sub sp, sp, #8
-    str w0, [sp, #4]
-    adr x0, int_format
-    mov x1, sp
-    bl printf
-    add sp, sp, #8
-    ret
-    
-input1:     .skip   1
-input2:     .skip   1
+        // Allocate stack space for input buffer and variables
+        sub sp, sp, #16
+
+        // Read integer 1
+        adrp x0, input1
+        add x0, x0, #:lo12:input1
+        mov w1, #2
+        mov w2, #0
+        bl scanf
+
+        // Read integer 2
+        adrp x0, input2
+        add x0, x0, #:lo12:input2
+        mov w1, #2
+        mov w2, #0
+        bl scanf
+
+        // Compare integers
+        ldrb w3, [input1]
+        ldrb w4, [input2]
+        cmp w3, w4
+        bge int1_is_larger
+
+        // Swap integers
+        mov w5, w3
+        mov w3, w4
+        mov w4, w5
+
+int1_is_larger:
+        // Print larger integer
+        adrp x0, str_larger
+        add x0, x0, #:lo12:str_larger
+        mov w1, #0
+        mov w2, #0
+        bl printf
+        adrp x0, int_fmt
+        add x0, x0, #:lo12:int_fmt
+        mov w1, #1
+        mov w2, w3
+        mul w2, w2, #10
+        add w2, w2, w4
+        bl printf
+
+        // Free stack space and exit
+        add sp, sp, #16
+        mov w0, #0
+        ret
+
+        // Data section
+        .data
+        input1: .byte 0,0
+        input2: .byte 0,0
+        str_larger: .asciz "The larger is "
+        int_fmt: .asciz "%d\n"
