2881c96f8e92d579929a9b3cd8f02923a358e3a3
mann <71485257+Mann1562@users.noreply.github.com>
2023-04-12 16:10:43 -0500

    Update larger.s

--- larger.s
+++ larger.s
@@ -1,58 +1,43 @@
-        .text
-        .global main
+    .global main
+    .text
 
 main:
-        // Allocate stack space for input buffer and variables
-        sub sp, sp, #16
+    // Prompt user to enter first integer
+    adr x0, input1_msg
+    bl printf
+    // Read first integer
+    adr x0, input_format
+    adr x1, input1
+    bl scanf
+    // Prompt user to enter second integer
+    adr x0, input2_msg
+    bl printf
+    // Read second integer
+    adr x0, input_format
+    adr x1, input2
+    bl scanf
+    // Compare the two integers
+    ldrb w0, [input1]
+    ldrb w1, [input2]
+    cmp w0, w1
+    bge greater_than_or_equal
+    // If the second integer is larger, print it
+    adr x0, output_msg
+    ldr x1, =input2
+    b print_value
+greater_than_or_equal:
+    // If the first integer is larger or equal, print it
+    adr x0, output_msg
+    ldr x1, =input1
+print_value:
+    bl printf
+    // Exit program
+    mov x0, #0
+    ret
 
-        // Read integer 1
-        adrp x0, input1
-        add x0, x0, #:lo12:input1
-        mov w1, #2
-        mov w2, #0
-        bl scanf
-
-        // Read integer 2
-        adrp x0, input2
-        add x0, x0, #:lo12:input2
-        mov w1, #2
-        mov w2, #0
-        bl scanf
-
-        // Compare integers
-        ldrb w3, [input1]
-        ldrb w4, [input2]
-        cmp w3, w4
-        bge int1_is_larger
-
-        // Swap integers
-        mov w5, w3
-        mov w3, w4
-        mov w4, w5
-
-int1_is_larger:
-        // Print larger integer
-        adrp x0, str_larger
-        add x0, x0, #:lo12:str_larger
-        mov w1, #0
-        mov w2, #0
-        bl printf
-        adrp x0, int_fmt
-        add x0, x0, #:lo12:int_fmt
-        mov w1, #1
-        mov w2, w3
-        mul w2, w2, #10
-        add w2, w2, w4
-        bl printf
-
-        // Free stack space and exit
-        add sp, sp, #16
-        mov w0, #0
-        ret
-
-        // Data section
-        .data
-        input1: .byte 0,0
-        input2: .byte 0,0
-        str_larger: .asciz "The larger is "
-        int_fmt: .asciz "%d\n"
+input1_msg: .asciz "Enter integer 1: "
+input2_msg: .asciz "Enter integer 2: "
+input_format: .asciz "%hhu"
+output_msg: .asciz "The larger is %hhu\n"
+input1: .space 1
+input2: .space 1
