06f6f8e1a4a0b2c80acc804df2f27bfc737894ee
mann <71485257+Mann1562@users.noreply.github.com>
2023-04-12 16:31:28 -0500

    Update larger.s

--- larger.s
+++ larger.s
@@ -1,60 +1,30 @@
-        .global main
-        
+    .data
+input1: .asciz "Enter the first number (two-digit integer): "
+input2: .asciz "Enter the second number (two-digit integer): "
+    .align 2
+    
+    .text
+    .global main
 main:
-        // set up stack pointer
-        mov sp, #0x8000
-        
-        // prompt user for input 1
-        ldr r0, =input1_prompt
-        bl printf
-        
-        // read input 1
-        ldr r0, =input_format
-        ldr r1, =input1
-        bl scanf
-        
-        // prompt user for input 2
-        ldr r0, =input2_prompt
-        bl printf
-        
-        // read input 2
-        ldr r0, =input_format
-        ldr r1, =input2
-        bl scanf
-        
-        // compare inputs
-        ldrb w0, [input1]
-        ldrb w1, [input2]
-        cmp w0, w1
-        bge output_input1
-        b output_input2
-        
-output_input1:
-        // output input 1 as the larger input
-        ldr r0, =output1_format
-        ldr r1, =input1
-        bl printf
-        b end_prog
-        
-output_input2:
-        // output input 2 as the larger input
-        ldr r0, =output2_format
-        ldr r1, =input2
-        bl printf
-        
-end_prog:
-        // exit program
-        mov r0, #0
-        mov r7, #1
-        svc 0
-        
-        // data section
-        .data
-        
-        input1_prompt: .asciz "Enter integer 1: "
-        input2_prompt: .asciz "Enter integer 2: "
-        input_format: .asciz "%d"
-        output1_format: .asciz "The larger is %d\n"
-        output2_format: .asciz "The larger is %d\n"
-        input1: .byte 0
-        input2: .byte 0
+    // prompt user to input the first number
+    adr x0, input1 // load the address of the input1 string into x0
+    bl printf // call the printf function to print the string
+    
+    // read the first number from standard input
+    ldr x0, =0 // set x0 to 0
+    bl scanf // call the scanf function to read the integer into x0
+    
+    // prompt user to input the second number
+    adr x0, input2 // load the address of the input2 string into x0
+    bl printf // call the printf function to print the string
+    
+    // read the second number from standard input
+    ldr x1, =0 // set x1 to 0
+    bl scanf // call the scanf function to read the integer into x1
+    
+    // rest of the program
+    // ...
+    
+    // exit program
+    mov x0, #0 // set x0 to 0
+    bl exit // call the exit function to exit the program
