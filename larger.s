bef72c5dcaaa3ec4160d91262385dd9219279cf2
mann <71485257+Mann1562@users.noreply.github.com>
2023-04-12 16:21:39 -0500

    Update larger.s

--- larger.s
+++ larger.s
@@ -1,79 +1,58 @@
 .data
-input1: .byte 0
-input2: .byte 0
-format_str: .asciz "Enter integer %d: "
-larger_str: .asciz "The larger is %d\n"
+input1: .asciz "Enter integer 1: "
+input2: .asciz "Enter integer 2: "
+output: .asciz "The larger is %d\n"
+
+.balign 4
+input1_value: .word 0
+input2_value: .word 0
 
 .text
 .global main
-
 main:
-    // Prompt for and read input1
-    mov w0, #1 // stdout
-    mov w1, #0 // file descriptor for stdin
-    mov w2, #0 // offset
-    adr x3, format_str // format string
-    mov w8, #1 // number of arguments
-    svc #0 // syscall for printf
-    adrp x0, input1 // address of input1
-    add x0, x0, :lo12:input1
-    mov w1, #2 // read stdin
-    mov w2, #0 // offset
-    mov w3, #2 // read 2 bytes
-    svc #0 // syscall for read
-    bne input1_error // check for error
-    ldrb w0, [input1] // load input1
-
-    // Prompt for and read input2
-    mov w0, #1 // stdout
-    mov w1, #0 // file descriptor for stdin
-    mov w2, #0 // offset
-    adr x3, format_str // format string
-    mov w8, #1 // number of arguments
-    svc #0 // syscall for printf
-    adrp x0, input2 // address of input2
-    add x0, x0, :lo12:input2
-    mov w1, #2 // read stdin
-    mov w2, #0 // offset
-    mov w3, #2 // read 2 bytes
-    svc #0 // syscall for read
-    bne input2_error // check for error
-    ldrb w1, [input2] // load input2
-
-    // Compare and print larger
-    cmp w0, w1 // compare input1 and input2
-    bge input1_is_larger // branch if input1 >= input2
-    mov w0, w1 // otherwise, set w0 to input2
-    b print_larger // branch to print_larger
-
-input1_is_larger:
-    b print_larger // branch to print_larger
-
-print_larger:
-    mov w1, #1 // stdout
-    adr x2, larger_str // format string
-    mov w8, #1 // number of arguments
-    svc #0 // syscall for printf
+    // prompt for input1
+    adr x0, input1
+    bl printf
+    
+    // read input1
+    adr x1, input1_value
+    bl scanf
+    
+    // prompt for input2
+    adr x0, input2
+    bl printf
+    
+    // read input2
+    adr x1, input2_value
+    bl scanf
+    
+    // load input1 into w0
+    ldrb w0, [input1_value]
+    msub w0, w0, w0, #10 // convert from ASCII to decimal
+    ldrb w1, [input1_value, #1]
+    msub w1, w1, w1, #10
+    add w0, w0, w1, lsl #1 // combine digits
+    
+    // load input2 into w1
+    ldrb w1, [input2_value]
+    msub w1, w1, w1, #10 // convert from ASCII to decimal
+    ldrb w2, [input2_value, #1]
+    msub w2, w2, w2, #10
+    add w1, w1, w2, lsl #1 // combine digits
+    
+    // compare inputs and print larger
+    cmp w0, w1
+    bge input1_larger
+    // input2 is larger
+    mov w0, w1
+    b print_output
+input1_larger:
+    // input1 is larger or equal
+print_output:
+    adr x0, output
+    mov x1, w0
+    bl printf
+    
+    // exit
+    mov x0, #0
     ret
-
-input1_error:
-    mov w0, #1 // stdout
-    adr x1, input1_error_str // error string
-    mov w8, #1 // number of arguments
-    svc #0 // syscall for printf
-    b exit_program
-
-input2_error:
-    mov w0, #1 // stdout
-    adr x1, input2_error_str // error string
-    mov w8, #1 // number of arguments
-    svc #0 // syscall for printf
-    b exit_program
-
-exit_program:
-    mov w0, #0 // exit status
-    mov w8, #0 // number of arguments
-    svc #0 // syscall for exit
-
-input1_error_str: .asciz "Error reading input1\n"
-input2_error_str: .asciz "Error reading input2\n"
