b91a48f4833cd49217fe13ec966015ca434e11d1
mann <71485257+Mann1562@users.noreply.github.com>
2023-04-12 16:41:48 -0500

    Update larger.s

--- larger.s
+++ larger.s
@@ -2,28 +2,30 @@
     inputMsg:   .asciz "Enter an integer: "
     outputMsg:  .asciz "You entered: %d\n"
     .balign 4
-    input:      .skip   4   @ Reserve 4 bytes for the input
+    input:      .word   0   @ Reserve 4 bytes for the input
 
     .text
     .global main
     main:
         @ Print the input prompt
-        ldr r0, =inputMsg
+        adr x0, inputMsg
         bl printf
 
-        @ Read the input as a string from stdin
-        ldr r0, =input
-        ldr r1, =4      @ Read up to 4 bytes (enough for a 32-bit integer)
-        bl fgets
+        @ Read the input integer from stdin
+        adr x0, input
+        mov x1, #10     @ Read up to 10 bytes (enough for a 32-bit integer)
+        mov x2, #0      @ Set the file descriptor for stdin
+        mov x8, #0      @ Set the read system call number
+        svc #0
 
         @ Convert the input string to an integer
-        ldr r0, =input
-        bl atoi
+        ldr w0, =input
+        ldr w0, [w0]
 
         @ Print the input integer
-        ldr r1, =outputMsg
-        mov r2, r0      @ Move the input integer to r2 for printing
+        adr x1, outputMsg
+        mov x2, w0      @ Move the input integer to x2 for printing
         bl printf
 
-        mov r0, #0      @ Return 0
-        bx lr
+        mov w0, #0      @ Return 0
+        ret
