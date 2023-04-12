7e6452db4a36553600b7f911a038f5b588a2e943
mann <71485257+Mann1562@users.noreply.github.com>
2023-04-12 16:24:41 -0500

    Update larger.s

--- larger.s
+++ larger.s
@@ -1,58 +1,94 @@
-.data
-input1: .asciz "Enter integer 1: "
-input2: .asciz "Enter integer 2: "
-output: .asciz "The larger is %d\n"
+@ This program reads two two-digit non-negative integers from standard input
+@ and prints the larger of the two to standard output.
 
-.balign 4
-input1_value: .word 0
-input2_value: .word 0
+    .global main
+    .extern printf
+    .extern scanf
 
-.text
-.global main
+    .section .data
+format_in:
+    .string "%d"
+format_out:
+    .string "The larger is %d\n"
+input1:
+    .skip 4
+input2:
+    .skip 4
+
+    .section .text
 main:
-    // prompt for input1
-    adr x0, input1
-    bl printf
-    
-    // read input1
-    adr x1, input1_value
+    @ Prompt the user for input 1
+    adr x0, format_in
     bl scanf
-    
-    // prompt for input2
-    adr x0, input2
-    bl printf
-    
-    // read input2
-    adr x1, input2_value
+    mov w1, 0
+    ldrb w0, [input1]
+    cmp w0, #'0'
+    b.lo input1_error
+    cmp w0, #'9'
+    b.hi input1_error
+    sub w0, w0, #'0'
+    mul w1, w1, 10
+    add w1, w1, w0
+
+    @ Prompt the user for input 2
+    adr x0, format_in
     bl scanf
-    
-    // load input1 into w0
-    ldrb w0, [input1_value]
-    msub w0, w0, w0, #10 // convert from ASCII to decimal
-    ldrb w1, [input1_value, #1]
-    msub w1, w1, w1, #10
-    add w0, w0, w1, lsl #1 // combine digits
-    
-    // load input2 into w1
-    ldrb w1, [input2_value]
-    msub w1, w1, w1, #10 // convert from ASCII to decimal
-    ldrb w2, [input2_value, #1]
-    msub w2, w2, w2, #10
-    add w1, w1, w2, lsl #1 // combine digits
-    
-    // compare inputs and print larger
-    cmp w0, w1
-    bge input1_larger
-    // input2 is larger
+    mov w2, 0
+    ldrb w0, [input2]
+    cmp w0, #'0'
+    b.lo input2_error
+    cmp w0, #'9'
+    b.hi input2_error
+    sub w0, w0, #'0'
+    mul w2, w2, 10
+    add w2, w2, w0
+
+    @ Compare the two inputs and print the larger
+    cmp w1, w2
+    b.gt print1
+    b.lt print2
+    b print1
+
+input1_error:
+    @ Error if input1 is not a two-digit non-negative integer
+    adr x0, format_out
+    mov w1, 0
+    mov w0, 0
+    bl printf
+    b exit
+
+input2_error:
+    @ Error if input2 is not a two-digit non-negative integer
+    adr x0, format_out
+    mov w1, 0
+    mov w0, 0
+    bl printf
+    b exit
+
+print1:
+    @ Print the value of input1 since it is larger
+    adr x0, format_out
+    mov w1, 0
     mov w0, w1
-    b print_output
-input1_larger:
-    // input1 is larger or equal
-print_output:
-    adr x0, output
-    mov x1, w0
+    ldrb w1, [input1]
+    sub w1, w1, #'0'
+    mul w0, w0, 10
+    add w0, w0, w1
     bl printf
-    
-    // exit
-    mov x0, #0
+    b exit
+
+print2:
+    @ Print the value of input2 since it is larger
+    adr x0, format_out
+    mov w1, 0
+    mov w0, w2
+    ldrb w1, [input2]
+    sub w1, w1, #'0'
+    mul w0, w0, 10
+    add w0, w0, w1
+    bl printf
+
+exit:
+    @ Exit the program
+    mov w0, 0
     ret
