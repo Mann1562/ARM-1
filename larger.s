cc7a4290a0b7323e7613e59cd4652d7f67cc3e4e
mann <71485257+Mann1562@users.noreply.github.com>
2023-04-12 16:12:59 -0500

    Update larger.s

--- larger.s
+++ larger.s
@@ -1,43 +1,68 @@
-    .global main
-    .text
+.data
+input1: .asciz "Enter integer 1: "
+input2: .asciz "Enter integer 2: "
+output: .asciz "The larger is %d\n"
 
+.text
+.global main
 main:
-    // Prompt user to enter first integer
-    adr x0, input1_msg
-    bl printf
-    // Read first integer
-    adr x0, input_format
-    adr x1, input1
-    bl scanf
-    // Prompt user to enter second integer
-    adr x0, input2_msg
-    bl printf
-    // Read second integer
-    adr x0, input_format
-    adr x1, input2
-    bl scanf
-    // Compare the two integers
-    ldrb w0, [input1]
-    ldrb w1, [input2]
-    cmp w0, w1
-    bge greater_than_or_equal
-    // If the second integer is larger, print it
-    adr x0, output_msg
-    ldr x1, =input2
-    b print_value
-greater_than_or_equal:
-    // If the first integer is larger or equal, print it
-    adr x0, output_msg
-    ldr x1, =input1
-print_value:
-    bl printf
-    // Exit program
-    mov x0, #0
-    ret
+    // set up stack
+    mov sp, x29
+    add x29, sp, #0
 
-input1_msg: .asciz "Enter integer 1: "
-input2_msg: .asciz "Enter integer 2: "
-input_format: .asciz "%hhu"
-output_msg: .asciz "The larger is %hhu\n"
-input1: .space 1
-input2: .space 1
+    // prompt for integer 1
+    adr x0, input1
+    bl printf
+    mov x0, #0
+    mov w1, #10
+    mov w2, #0
+loop1:
+    bl getchar
+    cmp w0, #'0'
+    blt cont1
+    cmp w0, #'9'
+    bgt cont1
+    msub w2, w2, w1, #0
+    add w2, w2, w0, #0
+    mul w0, w0, w1
+    add w0, w0, w2
+    b loop1
+cont1:
+    sub w0, w0, w2
+
+    // prompt for integer 2
+    adr x0, input2
+    bl printf
+    mov x0, #0
+    mov w1, #10
+    mov w2, #0
+loop2:
+    bl getchar
+    cmp w0, #'0'
+    blt cont2
+    cmp w0, #'9'
+    bgt cont2
+    msub w2, w2, w1, #0
+    add w2, w2, w0, #0
+    mul w0, w0, w1
+    add w0, w0, w2
+    b loop2
+cont2:
+    sub w0, w0, w2
+
+    // compare the two integers
+    cmp w0, w1
+    bgt first_larger
+    mov w0, w1
+first_larger:
+
+    // print the result
+    adr x0, output
+    mov x1, w0
+    bl printf
+
+    // exit the program
+    mov w0, #0
+    mov sp, x29
+    sub x29, x29, #8
+    ret
