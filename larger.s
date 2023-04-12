b0f425a3c5c065c95829c51ebc2ce6bf0d080c8d
mann <71485257+Mann1562@users.noreply.github.com>
2023-04-12 16:44:10 -0500

    Update larger.s

--- larger.s
+++ larger.s
@@ -1,8 +1,8 @@
     .data
-    inputMsg:   .asciz "Enter an integer: "
-    outputMsg:  .asciz "You entered: %d\n"
-    .balign 4
-    input:      .word   0   @ Reserve 4 bytes for the input
+    inputMsg:   .asciz "Enter a 64-bit integer: "
+    outputMsg:  .asciz "You entered: %ld\n"
+    .balign 8
+    input:      .xword  0   @ Reserve 8 bytes for the input
 
     .text
     .global main
@@ -13,18 +13,18 @@
 
         @ Read the input integer from stdin
         adr x0, input
-        mov x1, #10     @ Read up to 10 bytes (enough for a 32-bit integer)
+        mov x1, #20     @ Read up to 20 bytes (enough for a 64-bit integer)
         mov x2, #0      @ Set the file descriptor for stdin
         mov x8, #0      @ Set the read system call number
         svc #0
 
         @ Convert the input string to an integer
-        ldr w0, =input
-        ldr w0, [w0]
+        ldr x0, =input
+        ldr x0, [x0]
 
         @ Print the input integer
         adr x1, outputMsg
-        mov x2, w0      @ Move the input integer to x2 for printing
+        mov x2, x0      @ Move the input integer to x2 for printing
         bl printf
 
         mov w0, #0      @ Return 0
