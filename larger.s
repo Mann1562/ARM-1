1c399d28e327fdbfb5d5bc641ecd17e935077878
mann <71485257+Mann1562@users.noreply.github.com>
2023-04-12 16:17:07 -0500

    Update larger.s

--- larger.s
+++ larger.s
@@ -1,68 +1,53 @@
 .data
-input1: .asciz "Enter integer 1: "
-input2: .asciz "Enter integer 2: "
-output: .asciz "The larger is %d\n"
+input1: .ascii "Enter integer 1: "
+input2: .ascii "Enter integer 2: "
+output: .ascii "The larger is "
+
+.balign 4
+input1_buf: .skip 3
+input2_buf: .skip 3
 
 .text
 .global main
 main:
-    // set up stack
-    mov sp, x29
-    add x29, sp, #0
-
-    // prompt for integer 1
-    adr x0, input1
-    bl printf
-    mov x0, #0
-    mov w1, #10
-    mov w2, #0
-loop1:
-    bl getchar
-    cmp w0, #'0'
-    blt cont1
-    cmp w0, #'9'
-    bgt cont1
-    msub w2, w2, w1, #0
-    add w2, w2, w0, #0
-    mul w0, w0, w1
-    add w0, w0, w2
-    b loop1
-cont1:
-    sub w0, w0, w2
-
-    // prompt for integer 2
-    adr x0, input2
-    bl printf
-    mov x0, #0
-    mov w1, #10
-    mov w2, #0
-loop2:
-    bl getchar
-    cmp w0, #'0'
-    blt cont2
-    cmp w0, #'9'
-    bgt cont2
-    msub w2, w2, w1, #0
-    add w2, w2, w0, #0
-    mul w0, w0, w1
-    add w0, w0, w2
-    b loop2
-cont2:
-    sub w0, w0, w2
-
-    // compare the two integers
-    cmp w0, w1
-    bgt first_larger
-    mov w0, w1
-first_larger:
-
-    // print the result
-    adr x0, output
-    mov x1, w0
-    bl printf
-
-    // exit the program
-    mov w0, #0
-    mov sp, x29
-    sub x29, x29, #8
-    ret
+  // Print prompt for integer 1
+  adr x0, input1
+  bl printf
+  // Read integer 1
+  adr x0, input1_buf
+  mov x1, #3
+  bl fgets
+  ldrb w0, [input1_buf]
+  sub w0, w0, #'0'
+  msub w0, w0, w0, #10
+  // Print prompt for integer 2
+  adr x0, input2
+  bl printf
+  // Read integer 2
+  adr x0, input2_buf
+  mov x1, #3
+  bl fgets
+  ldrb w1, [input2_buf]
+  sub w1, w1, #'0'
+  msub w1, w1, w1, #10
+  // Compare integers and print result
+  cmp w0, w1
+  bge int1_greater
+  // Integer 2 is greater
+  adr x0, output
+  bl printf
+  ldrb w0, [input2_buf]
+  sub w0, w0, #'0'
+  msub w0, w0, w0, #10
+  b exit_prog
+int1_greater:
+  // Integer 1 is greater
+  adr x0, output
+  bl printf
+  ldrb w0, [input1_buf]
+  sub w0, w0, #'0'
+  msub w0, w0, w0, #10
+exit_prog:
+  // Exit program
+  mov x0, #0
+  ret
