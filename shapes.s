e68a1b5bdd13f25e27c609c7254c09c9e8d35301
mann <71485257+Mann1562@users.noreply.github.com>
2023-04-11 20:46:54 -0500

    Update shapes.s

--- shapes.s
+++ shapes.s
@@ -1,4 +1,4 @@
-// MANN PATEL!!!!!
+// PUT YOUR NAME HERE!!!!!
 //
 // Assembler program to print required strings to stdout and set program return code
 //
@@ -16,18 +16,18 @@ _start: mov	X0, #1			// 1 = StdOut
 		svc	0				// Call linux to output the string
 
 // Setup the parameters to set the program return code and terminate the program
-		mov     X0, #9		// Sum of the individual digits in my TCU ID number
+		mov     X0, #29		// Sum of the individual digits in my TCU ID number
 		mov     X8, #93		// Service command code 93 terminates this program
 		svc     0			// Call linux to terminate the program
 
 .data
 string:	.ascii "0000000001111111111222222222233333333333\n"
-		.ascii "1234567890123456789012345678901234567890\n"
-		.ascii "+------+           =               @\n"
-		.ascii "|               |         = =             @@\n"
-		.ascii "|               |       =     =           @@@\n"
-		.ascii "|               |      =       =          @@@@\n"
-		.ascii "|               |       =     =           @@@@@\n"
-		.ascii "|               |         = =             @@@@@@\n"
-		.ascii "|               |           =               @@@@@@@\n"
-		.ascii "+------+                            @@@@@@@@\n"
+	.ascii "1234567890123456789012345678901234567890\n"
+	.ascii "+------+        =        @\n"
+	.ascii "|      |       = =       @@\n"
+	.ascii "|      |      =   =      @@@\n"
+	.ascii "|      |     =     =     @@@@\n"
+	.ascii "|      |      =   =      @@@@@\n"
+	.ascii "|      |       = =       @@@@@@\n"
+	.ascii "|      |        =        @@@@@@@\n"
+	.ascii "+------+                 @@@@@@@@\n"
