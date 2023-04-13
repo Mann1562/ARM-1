351336e4293b07137e8722237c35e02397f5caa7
mann <71485257+Mann1562@users.noreply.github.com>
2023-04-13 17:45:04 -0500

    Update larger.s

--- larger.s
+++ larger.s
@@ -2,26 +2,46 @@
 num1: .word 0
 num2: .word 0
 prompt: .asciz "Enter a number: "
-result: .asciz "The larger number is: %d \n"
+result: .asciz "The larger number is: %d\n"
 
 .text
 .global _start
 
 _start:
-adr x0, prompt
-mov x1, 0
-mov x2, 16
-mov x8, 0
-svc 0
+    // Prompt for and read in the first number
+    adr x0, prompt
+    mov x1, 0
+    mov x2, 16
+    mov x8, 0
+    svc 0
+    ldr x1, [num1]
+    
+    // Prompt for and read in the second number
+    adr x0, prompt
+    mov x1, 0
+    mov x2, 16
+    mov x8, 0
+    svc 0
+    ldr x2, [num2]
+    
+    // Compare the two numbers and print the larger one
+    cmp x1, x2
+    bge num1_is_larger
+    ldr x0, =num2
+    b print_result
+    
+num1_is_larger:
+    ldr x0, =num1
 
-str x0, [num1]
-
-adr x0, prompt
-mov x1, x0
-mov x2, 16
-mov x8, 0
-svc 0
-
-str x0, [num2]
-
-
+print_result:
+    ldr x1, [x0]
+    adr x0, result
+    mov x2, x1
+    mov x1, 1
+    mov x8, 4
+    svc 0
+    
+exit:
+    mov x0, 0
+    mov x8, 93
+    svc 0
