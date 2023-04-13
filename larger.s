9c94621c35098d735628ebe25ec6c620c103aa48
mann <71485257+Mann1562@users.noreply.github.com>
2023-04-13 17:52:04 -0500

    Update larger.s

--- larger.s
+++ larger.s
@@ -1,47 +1,48 @@
 .data
-num1: .word 0
-num2: .word 0
 prompt: .asciz "Enter a number: "
-result: .asciz "The larger number is: %d\n"
+result: .asciz "The larger number is: %d \n"
 
 .text
 .global _start
 
 _start:
-    // Prompt for and read in the first number
+    // read num1 into register x1
     adr x0, prompt
     mov x1, 0
     mov x2, 16
     mov x8, 0
     svc 0
-    ldr x1, [num1]
-    
-    // Prompt for and read in the second number
-    adr x0, prompt
-    mov x1, 0
-    mov x2, 16
-    mov x8, 0
-    svc 0
-    ldr x2, [num2]
-    
-    // Compare the two numbers and print the larger one
-    cmp x1, x2
-    bge num1_is_larger
-    ldr x0, =num2
-    b print_result
-    
-num1_is_larger:
-    ldr x0, =num1
 
-print_result:
-    ldr x1, [x0]
-    adr x0, result
-    mov x2, x1
-    mov x1, 1
-    mov x8, 4
+    // read num2 into register x2
+    adr x0, prompt
+    mov x1, 0
+    mov x2, 16
+    mov x8, 0
     svc 0
-    
+
+    // compare num1 and num2 and print the larger value
+    cmp x1, x2
+    bge print_num1
+    b print_num2
+
+print_num1:
+    adr x0, result
+    mov x1, x1
+    mov x2, 0
+    mov x8, 1
+    svc 0
+    b exit
+
+print_num2:
+    adr x0, result
+    mov x1, x2
+    mov x2, 0
+    mov x8, 1
+    svc 0
+    b exit
+
 exit:
+    // exit the program
     mov x0, 0
     mov x8, 93
     svc 0
