d59ed1b0b6d7266c9e0682c180f5a879f285760e
mann <71485257+Mann1562@users.noreply.github.com>
2023-04-13 19:19:37 -0500

    Update larger.s

--- larger.s
+++ larger.s
@@ -1,41 +1,43 @@
-		.section .data
+	.section .data
 prompt1: .ascii "Enter integer 1: \n"
 prompt2: .ascii "Enter integer 2: \n"
 result:  .ascii "The larger is "
 num1: .word 4
 num2: .word 4
-		.section .text
-		.global _start	
+
+	.section .text
+	.global _start	
+
 _start:
-    // Print prompt1
+    // Print the first prompt for user input (prompt1)
     mov x0, #1
     ldr x1, =prompt1
     mov x2, #16
     mov x8, #64
 	svc 0
 
-    // Read num1
+    // Read first input number (num1)
     mov x0, #0
     adr x1, num1 
     mov x2, #4
     mov x8, #63
     svc 0
 
-    // Print prompt2
+    // Print the second prompt for user input (prompt2)
     mov x0, #1
     ldr x1, =prompt2
     mov x2, #16
     mov x8, #64
 	svc 0
 
-    // Read num2
+    // Read second input number (num2)
     mov x0, #0
     adr x1, num2
     mov x2, #16
     mov x8, #63
     svc 0
 	
-	//Print the result_line
+	//Prints the result line (result)
 	mov	x0, #1
 	ldr x1, =result
 	mov	x2, #14
@@ -43,16 +45,16 @@ _start:
 	svc 0
 
 
-    // Compare num1 and num2
+    // Compare the 2 2 digit numbers
     ldr x3, =num1       // Load num1 into x3
     ldr w4,	[x3]
     ldr x5, =num2    	// Load num2 into x4
     ldr	w6, [x5]
     cmp w4, w6
-	blt print_larger_num2
+	blt print_num2
 	
 
-print_larger_num1:
+print_num1:
     // Print the result with num2 as the larger integer
     mov x0, #1
     ldr x1, =num1
@@ -61,7 +63,7 @@ print_larger_num1:
     svc 0
     b exit
 
-print_larger_num2:
+print_num2:
     // Print the result with num1 as the larger integer
     mov x0, #1
     ldr x1, =num2
