91ffc925c0ab47cf8180511ae3dc3d570d0a57f3
mann <71485257+Mann1562@users.noreply.github.com>
2023-04-25 13:28:00 -0500

    Update intsort.s

--- intsort.s
+++ intsort.s
@@ -1,47 +1,51 @@
-.global main
-    .bss
-array:
-    .skip 40
-    .text
-main:
-    mov x0, #0           // loop counter
-    ldr x1, =array       // load the address of the array
-    ldr x2, =RAND_MAX    // load the value of RAND_MAX
-    ldr x3, =MIN_VALUE   // load the value of MIN_VALUE
-loop:
-    cmp x0, #10          // compare loop counter with 10
-    bge done             // if >= 10, exit the loop
-    bl srand             // seed the random number generator
-    bl rand              // generate a random number
-    umod x7, x0, #90     // calculate the random number in the range of [0,89]
-    add x7, x7, #10      // add 10 to get the random number in the range of [10,99]
-    add x7, x7, x3       // add MIN_VALUE to make the number positive
-    str x7, [x1, x0, lsl #2] // store the number in the array
-    add x0, x0, #1       // increment loop counter
-    b loop               // repeat the loop
-done:
-    mov x0, #0           // loop counter
-    ldr x1, =array       // load the address of the array
-print:
-    cmp x0, #10          // compare loop counter with 10
-    bge exit             // if >= 10, exit the loop
-    ldr x7, [x1, x0, lsl #2] // load the number from the array
-    mov x0, #0           // set format string for printf
-    mov w0, #1
-    adr x1, msg
-    mov w2, #5
-    svc 0
-    add x0, x0, #1
-    adr x1, space
-    mov w2, #1
-    svc 0
-    add x0, x0, #1
-    add x0, x0, #1
-    b print              // repeat the loop
-exit:
-    mov w0, #0           // exit program
-    mov w8, #93
-    svc 0
+    .section .data
+    .section .bss
+    .lcomm array, 40             // reserve 40 bytes for the array (10 integers * 4 bytes per integer)
 
-msg:    .ascii "%5d"
-space:  .ascii " "
+    .section .text
+    .global main
+    .import rand
+    .import printf
+
+main:
+    // Seed the random number generator with the current time
+    adr x0, current_time
+    bl srand
+
+    // Generate 10 random integers in the range [10, 99] and store them in the array
+    mov x1, #0                  // i = 0
+loop:
+    ldr x2, =array              // load address of array into x2
+    add x2, x2, x1, lsl #2      // calculate address of array[i]
+    bl rand                     // generate a random integer
+    mov x3, #90                 // maximum random value = 99 - 10 + 1 = 90
+    add x4, x0, #10             // minimum random value = 10
+    sdiv x0, x0, x3             // divide the random number by 90
+    msub x0, x0, x3, x0         // multiply by 90 and subtract from original value to get the remainder
+    add x0, x0, x4              // add 10 to get a random value in the range [10, 99]
+    str x0, [x2]                // store the random value in array[i]
+    add x1, x1, #1              // i++
+    cmp x1, #10
+    b.lt loop
+
+    // Print the array
+    ldr x0, =msg
+    bl printf
+    ldr x0, =array
+    mov x1, #10
+print_loop:
+    ldr w2, [x0], #4
+    mov w1, #5                  // format specifier for printing integers with a width of 5
+    bl printf
+    sub x1, x1, #1
+    cbnz x1, print_loop
+
+    // Exit
+    mov x0, #0
+    ret
+
+    .section .data
+msg:
+    .asciz "The random array is: "
+current_time:
+    .space 8
