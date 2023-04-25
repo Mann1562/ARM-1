c6b13854832a2001c4a8a7a907ce772e6e96d5dd
mann <71485257+Mann1562@users.noreply.github.com>
2023-04-25 13:25:45 -0500

    Update intsort.s

--- intsort.s
+++ intsort.s
@@ -1,38 +1,47 @@
-// Declare external symbols for C library functions
-.extern printf, srand, rand
-.section .data
-  // Define constants
-  .equ ARRAY_SIZE, 10
-  .equ MIN_VALUE, 10
-  .equ MAX_VALUE, 99
-  // Declare array of 10 integers in the .bss section
-  .lcomm array, ARRAY_SIZE * 4
-.section .text
-  .global main
+.global main
+    .bss
+array:
+    .skip 40
+    .text
 main:
-  // Set up stack frame
-  stp x29, x30, [sp, -16]!
-  mov x29, sp
-  // Seed random number generator
-  bl srand
-  // Fill array with random integers in the range [10..99]
-  mov x0, #MAX_VALUE-MIN_VALUE+1
-  mov x1, #0
-  bl rand
-  add x1, x0, #MIN_VALUE
-  adr x2, array
-  ldr x3, =ARRAY_SIZE
-fill_loop:
-  str w1, [x2], 4
-  subs x3, x3, #1
-  bne fill_loop
-  // Print array to standard output
-  adr x0, fmt
-  adr x1, array
-  bl printf
-  // Clean up stack frame and return
-  ldp x29, x30, [sp], 16
-  ret
-  // Define format string for printf
-fmt:
-  .asciz "%d "
+    mov x0, #0           // loop counter
+    ldr x1, =array       // load the address of the array
+    ldr x2, =RAND_MAX    // load the value of RAND_MAX
+    ldr x3, =MIN_VALUE   // load the value of MIN_VALUE
+loop:
+    cmp x0, #10          // compare loop counter with 10
+    bge done             // if >= 10, exit the loop
+    bl srand             // seed the random number generator
+    bl rand              // generate a random number
+    umod x7, x0, #90     // calculate the random number in the range of [0,89]
+    add x7, x7, #10      // add 10 to get the random number in the range of [10,99]
+    add x7, x7, x3       // add MIN_VALUE to make the number positive
+    str x7, [x1, x0, lsl #2] // store the number in the array
+    add x0, x0, #1       // increment loop counter
+    b loop               // repeat the loop
+done:
+    mov x0, #0           // loop counter
+    ldr x1, =array       // load the address of the array
+print:
+    cmp x0, #10          // compare loop counter with 10
+    bge exit             // if >= 10, exit the loop
+    ldr x7, [x1, x0, lsl #2] // load the number from the array
+    mov x0, #0           // set format string for printf
+    mov w0, #1
+    adr x1, msg
+    mov w2, #5
+    svc 0
+    add x0, x0, #1
+    adr x1, space
+    mov w2, #1
+    svc 0
+    add x0, x0, #1
+    add x0, x0, #1
+    b print              // repeat the loop
+exit:
+    mov w0, #0           // exit program
+    mov w8, #93
+    svc 0
+
+msg:    .ascii "%5d"
+space:  .ascii " "
