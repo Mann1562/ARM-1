be329cd5380ec3371adfc98766e156cbdde99453
mann <71485257+Mann1562@users.noreply.github.com>
2023-04-26 16:42:23 -0500

    Update intsort.s

--- intsort.s
+++ intsort.s
@@ -1,41 +1,93 @@
 .global main
-    .text
 
+.section .bss
+array: .space 40  // Reserve space for an array of 10 integers (4 bytes each)
+
+.section .data
+str_random_array: .asciz "The random array is:"
+str_sorted_array: .asciz "The sorted array is:"
+str_int_format: .asciz " %5d"
+str_newline: .asciz "\n"
+
+.section .text
 main:
-    // Initialize random number generator
-    mov x0, #0       // Use current time as seed
-    bl srand
+    stp x29, x30, [sp, #-16]!  // Save frame pointer and link register
 
-    // Allocate space for array
-    mov x1, #10      // Number of elements in array
-    lsl x1, x1, #2   // Multiply by 4 to get size in bytes
-    mov x2, #0       // Alignment
-    mov x0, #0       // Flags
-    mov x8, #215     // System call number for mmap
-    mov x16, #0      // File descriptor (not used)
-    mov x17, #0x22   // PROT_READ | PROT_WRITE | PROT_EXEC
-    mov x18, #0x22   // MAP_PRIVATE | MAP_ANONYMOUS
-    svc #0
+    // Fill the array with 10 random integers in the range [10..99]
+    mov x19, 10
+    ldr x20, =array
+generate_random_numbers:
+    bl rand
+    and w0, w0, 0x7F
+    cmp w0, 99
+    b.gt generate_random_numbers
+    cmp w0, 10
+    b.lt generate_random_numbers
+    str w0, [x20], 4
+    subs x19, x19, 1
+    b.ne generate_random_numbers
 
-    // Initialize array with random numbers
-    mov x3, x0       // Base address of array
-    mov x4, #10      // Number of elements
-loop:
-    bl rand          // Generate random number
-    and x5, x0, #0x7F  // Limit to range 0-127
-    add x5, x5, #10  // Shift range to 10-137
-    str w5, [x3], #4 // Store random number in array
-    subs x4, x4, #1
-    bne loop
+    // Print the random array
+    ldr x0, =str_random_array
+    bl printf
+    mov x19, 10
+    ldr x20, =array
+    
+print_random_array:
+    ldr w1, [x20], 4
+    ldr x0, =str_int_format
+    bl printf
+    subs x19, x19, 1
+    b.ne print_random_array
+    ldr x0, =str_newline
+    bl printf
 
-    // Print array to standard output
-    mov x0, #1       // File descriptor for stdout
-    mov x1, x3       // Base address of array
-    lsl x2, #2       // Multiply by 4 to get size in bytes
-    mov x8, #64      // System call number for write
-    svc #0
+    // Insertion sort algorithm
+    mov x19, 1
+    
+start_insertion_sort:
+    cmp x19, 9
+    b.gt finish_insertion_sort
+    ldr x20, =array
+    add x20, x20, x19, lsl 2
+    ldr w21, [x20]
+    sub x22, x19, 1
+    sub x20, x20, 4
+    
+shift_elements:
+    cmp x22, 0
+    b.lt insert_key
+    ldr w23, [x20]
+    cmp w23, w21
+    b.le insert_key
+    add x24, x20, 4
+    str w23, [x24]
+    sub x20, x20, 4
+    sub x22, x22, 1
+    b shift_elements
+    
+insert_key:
+    add x24, x20, 4
+    str w21, [x24]
+    add x19, x19, 1
+    b start_insertion_sort
 
-    // Exit program
-    mov x0, #0       // Return value
-    mov x8, #93      // System call number for exit
-    svc #0
+finish_insertion_sort:
+    // Print the sorted array
+    ldr x0, =str_sorted_array
+    bl printf
+    mov x19, 10
+    ldr x20, =array
+    
+print_sorted_array:
+    ldr w1, [x20], 4
+    ldr x0, =str_int_format
+    bl printf
+    subs x19, x19, 1
+    b.ne print_sorted_array
+    ldr x0, =str_newline
+    bl printf
+
+    // Exit the program properly
+    mov w0, 0
+    bl exit
