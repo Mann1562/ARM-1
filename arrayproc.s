cba3dad9413b523f0dc7373d6db077dd00dcb790
mann <71485257+Mann1562@users.noreply.github.com>
2023-05-04 15:20:35 -0500

    Update arrayproc.s

--- arrayproc.s
+++ arrayproc.s
@@ -1,232 +1,261 @@
-init_array:
-    stp x29, x30, [sp, #-16]!
-    mov x29, sp
 
-    // x0 - address of array
-    // x1 - size of array
-    mov x2, #255    // upper bound
-    mov x3, #0      // lower bound
-loop:
-    mov w4, #0
-    bl rand
-    umod w4, w4, #256  // w4 = rand() % 256
-    strb w4, [x0], #1
-    subs x1, x1, #1
-    bne loop
+        .section .rodata
+getIdStr: .string "Enter your TCU ID number: "
+        .align 3
+inputintstr: .asciz "%d"
+        .align 3
+intstr: .asciz "\t%d"
+        .align 3
+newline: .asciz "\n"
+        .align 3
+outputstr1: .asciz "\nCreating 2 arrays of %d values each.\n\n"
+        .align 3
+outputstr2: .asciz "The original array is:\n"
+        .align 3
+outputstr3: .asciz "The sorted duplicate array is:\n"
+        .align 3
+outputstr4: .asciz "The average of the duplicate array is: %d\n"
+        .align 3
 
-    mov sp, x29
-    ldp x29, x30, [sp], #16
-    ret
 
-print_array:
-    stp x29, x30, [sp, #-16]!
-    mov x29, sp
 
-    // x0 - address of array
-    // x1 - size of array
-    mov w2, #0      // counter
-loop:
-    ldrb w3, [x0], #1
-    mov x4, x2
-    add w4, w4, #1
-    cmp w4, #5
-    b.eq print_and_reset
-    bne continue_loop
-print_and_reset:
-    mov w4, #0
-    bl printf
-    mov x5, #"\t"
-    bl printf
-    b continue_loop
-continue_loop:
-    subs x1, x1, #1
-    bne loop
 
-    // print the last line
-    mov w4, #0
-    bl printf
+		.section .bss
+id: .skip 4
+n: .skip 4
+n16: .skip 4
 
-    mov sp, x29
-    ldp x29, x30, [sp], #16
-    ret
 
-copy_array:
-    stp x29, x30, [sp, #-16]!
-    mov x29, sp
 
-    // x0 - address of destination array
-    // x1 - address of source array
-    // x2 - size of array
-loop:
-    ldr w3, [x1], #4
-    str w3, [x0], #4
-    subs x2, x2, #1
-    bne loop
-
-    mov sp, x29
-    ldp x29, x30, [sp], #16
-    ret
-
-insertion_sort:
-    // function prologue
-    stp x29, x30, [sp, -16]!
-    mov x29, sp
-    
-    // load parameters
-    ldr x1, [x29, #8]  // arr
-    ldr w2, [x29, #12] // n
-    
-    // sort the array
-    mov w3, #1 // start with second element
-    .sort_loop:
-        cbz w3, .sort_done // finished sorting
-        mov w4, w3 // current element index
-        mov w5, w4 // previous element index
-        sub w5, w5, #1
-        .shift_loop:
-            cbz w5, .shift_done // done shifting
-            ldr w6, [x1, w5, lsl #2] // previous element value
-            ldr w7, [x1, w4, lsl #2] // current element value
-            cmp w6, w7
-            b.le .shift_done // finished shifting
-            str w6, [x1, w4, lsl #2] // swap values
-            str w7, [x1, w5, lsl #2]
-            sub w4, w4, #1
-            sub w5, w5, #1
-            b .shift_loop
-        .shift_done:
-            sub w3, w3, #1
-            b .sort_loop
-    .sort_done:
-    
-    // function epilogue
-    ldp x29, x30, [sp], 16
-    ret
-
-// Compute the integer average of array 'arr' with 'n' values.
-// Input: arr: address of array
-// n: size of the array
-// Return: integer average (int)
-average:
-    stp x29, x30, [sp, #-16]!   // Save x29 and x30 on stack
-    mov x29, sp                 // Set up frame pointer
-
-    mov x0, x1                  // Pass address of array to sum_array
-    mov x1, #0                  // Pass startidx = 0 to sum_array
-    mov x2, x21                 // Pass stopidx = n-1 to sum_array
-    bl sum_array                // Call sum_array to compute the sum
-
-    sdiv x0, x0, x21            // Divide the sum by n
-    // The result is in x0
-
-    ldnp x29, x30, [sp], #16    // Restore x29 and x30 from stack
-    ret                         // Return
-
-sum_array:
-    stp x29, x30, [sp, -16]!  // Save the frame pointer and link register on the stack
-    mov x29, sp  // Set the frame pointer to the current stack pointer
-
-    // Compare the start and stop indices
-    cmp x1, x2
-    beq base_case  // If they are equal, we have reached the base case and should return arr[startidx]
-
-    // Otherwise, divide the array into two halves and recursively sum each half
-    add x3, x1, x2  // Compute the midpoint index
-    lsr x3, x3, #1
-    add x4, x3, #1  // Compute the starting index for the second half
-    bl sum_array    // Recursively sum the first half (startidx to midpoint)
-    mov x5, x0      // Save the result of the first recursive call in x5
-    mov x1, x4      // Set the start index for the second recursive call to x4
-    bl sum_array    // Recursively sum the second half (midpoint+1 to stopidx)
-    add x0, x5, x0  // Add the results of the two recursive calls together
-
-    // Restore the frame pointer and link register and return
-    ld1 {x29, x30}, [sp], #16
-    ret
-
-base_case:
-    ldr w0, [x0, x1, lsl #2]  // Load the value at arr[startidx] into w0
-    ld1 {x29, x30}, [sp], #16  // Restore the frame pointer and link register
-    ret
-
-.text
-.global main
+        .section .text
+        .global main
 main:
-  // Prompt user to enter TCU ID
-  adr x0, message
-  bl printf
-  // Read TCU ID from user input
-  adrp x0, buf
-  add x0, x0, #:lo12:buf
-  mov x1, #20 // read up to 20 characters
-  bl fgets
-  // Convert input to integer
-  ldr x1, buf
-  bl atoi
-  mov x2, x0 // x2 = n
-  // Set n according to TCU ID
-  and x0, x0, #1 // check if TCU ID is even or odd
-  cbz x0, evenID // if even
-  mov x2, #33 // if odd
-  b testFunctions
+    // function prolog
+	stp	x29, x30, [sp, #-16]! // store onto stack and subtract 16 immediate
+	// seed the random number generator here...
+	mov x0, #0
+    bl time
+    bl srand
+	// print and get the array size
+	// printf(getnstr)
+	ldr	x0, =getIdStr
+	bl	printf
+	// scanf(intstr, &n)
+	ldr	x0, =inputintstr
+	ldr	x1, =id
+	bl	scanf
+	// chck if id is even or odd
+	ldr x0, =id
+	ldr w0, [x0] // w0 = id
+	mov w2, #2 // w2 = 2
+    udiv w1, w0, w2 // w1 = w0 / w2 = id / 2
+    msub w0, w1, w2, w0 // w0 = w0 - w1 * w2 = id % 2
+    mov w2, #0
+    cmp w0, w2 // if w0 is even
+    b.eq remainder0 // then branch to remainder0
+remainder1: // odd
+	mov w0, #33
+	ldr x1, =n
+	str w0, [x1] // store 33 into n
+	b continueMain
+remainder0: // even
+	mov w0, #42
+	ldr x1, =n
+	str w0, [x1] // store 42 into n
+continueMain:
+	// compute the next highest multiple of 16 >= n
+	ldr	x1, =n
+	ldr	w1, [x1] // get value of n into w1
+	sbfiz	x1, x1, #2, #20 // set bits other than 2 to 21 to 0, signed extend
+	add	x1, x1, #0xf
+	and	x1, x1, #0xfffffffffffffff0
+	ldr	x2, =n16
+	str	w1, [x2] // store value of n into n16
+	// (b) create the storage for the two arrays
+    ldr x0, =outputstr1
+    ldr x1, =n
+    ldr w1, [x1]
+    bl printf
+    ldr	x1, =n16
+    ldr	x1, [x1]
+	sub	sp, sp, x1 // create storage for the array
+    mov x25, sp // store base address of first array into x25
+    ldr	x26, =n
+    ldr	w26, [x26] // store value of n into w26
+    sub sp, sp, x1 // create storage for the second array
+    mov x27, sp // store base address of second array into x27
+    ldr	x28, =n
+    ldr	w28, [x28] // store value of n into w28
+	// (c) call init_array for n integers
+	mov	x19, x25 // base of array1
+	mov x20, x26 // length of array1
+	bl	init_array
+    // (d) call print_array for array1
+    ldr x0, =outputstr2
+    bl printf
+    mov	x19, x25 // base of array1
+	mov x20, x26 // length of array1
+	bl	print_array
+    ldr x0, =newline
+    bl printf
+    // (e) call copy_array and copy array starting at x25 to x27
+    mov	x19, x27 // dest
+    mov	x20, x25 // src
+    mov	x21, x26 // n
+    bl	copy_array
+    // (f) call insertion_sort on the copied array
+    mov x19, x27 // base of array2
+    mov x20, x28 // length of array2
+    bl insertion_sort
+	// (g) call print_array on the sorted array
+    ldr x0, =outputstr3
+    bl printf
+    mov	x19, x27
+    mov x20, x28
+	bl	print_array
+    ldr x0, =newline
+    bl printf
+    // (h) call compute_average function on array2
+    mov x19, x27
+    mov x20, x28
+    bl compute_average
+    mov x1, x0
+    ldr x0, =outputstr4
+    bl printf
 
-evenID:
-  mov x2, #42 // if even
 
-testFunctions:
-  // Allocate memory on stack for orig and dup arrays
-  lsl x3, x2, #2 // sizeof(int) * n
-  sub sp, sp, x3 // make space for orig
-  sub sp, sp, x3 // make space for dup
-  mov x4, sp // x4 points to orig array
-  add x5, sp, x3 // x5 points to dup array
-  // Initialize orig array
-  mov x0, x4 // first argument: pointer to orig
-  mov x1, x2 // second argument: n
-  bl init_array
-  // Print orig array
-  adr x0, origMessage
-  bl printf
-  mov x0, x4 // first argument: pointer to orig
-  mov x1, x2 // second argument: n
-  bl print_array
-  // Copy orig array to dup array
-  mov x0, x5 // first argument: pointer to dup
-  mov x1, x4 // second argument: pointer to orig
-  mov x2, x3 // third argument: size of arrays
-  bl copy_array
-  // Sort dup array using insertion sort
-  mov x0, x5 // first argument: pointer to dup
-  mov x1, x2 // second argument: size of array
-  bl insertion_sort
-  // Print sorted dup array
-  adr x0, dupMessage
-  bl printf
-  mov x0, x5 // first argument: pointer to dup
-  mov x1, x2 // second argument: size of array
-  bl print_array
-  // Compute average of dup array
-  mov x0, x5 // first argument: pointer to dup
-  mov x1, x2 // second argument: size of array
-  bl compute_average
-  // Print average
-  adr x0, avgMessage
-  mov x1, x0 // first argument: pointer to string
-  mov x2, d0 // second argument: average value
-  bl printf
-  // Clean up and exit
-  add sp, sp, x3 // free space for dup
-  add sp, sp, x3 // free space for orig
-  mov x0, #0
-  ret
+    // return the local array back to the stack
+	ldr	x1, =n16
+	ldr	x1, [x1]
+	add	sp, sp, x1
+    add sp, sp, x1
+	// function epilog
+	ldp	x29, x30, [sp], #16
+	mov	x0, #0
+	ret
 
-message:
-  .ascii "Enter your TCU ID number: "
-buf:
-  .skip 21
-origMessage:
-  .ascii "The original array is:\n"
-dupMessage:
-  .ascii "The sorted duplicate array is:\n"
-avgMessage:
-  .ascii "The average of the duplicate array is: %d\n"
+        .type init_array, @function
+// void init_array(int arr[] - x19, int n - x20);
+init_array:
+	stp	x29, x30, [sp, #-16]! // function prolog
+	mov	x21, #0 // i at x21
+loop1:
+	cmp	x21, x20 // i >= n
+	bge	endloop1 // goto endloop1
+    bl rand
+    mov w2, #256 // w2 = 256
+    udiv w1, w0, w2 // w1 = w0 / w2 = rand() / 256
+    msub w0, w1, w2, w0 // w0 = w0 - w1 * w2 = rand() % 256
+	str	w0, [x19, x21, lsl #2] // store at base + index * 4
+	add	x21, x21, #1 // i++
+	b	loop1	
+endloop1:
+	ldp	x29, x30, [sp], #16 // function epilog
+	ret // return
+
+        .type print_array, @function
+// void print_array(int arr[] - x19, int n - x20);
+print_array:
+	stp	x29, x30, [sp, #-16]! // function prolog
+	mov	x21, #0 // i at x21
+loop2:
+	cmp	x21, x20 // i >= n
+	bge	endloop2 // goto endloop2
+    ldr x0, =intstr // load intstr into x0
+    ldr w1, [x19, x21, lsl #2] // read value at base + index * 4
+    bl printf // printf(x0, x1) = printf("%d", arr[i])
+    mov w0, #5 // w0 = 5
+    udiv w1, w21, w0 // w1 = w21 / w0 = i / 5
+    msub w0, w1, w0, w21 // w0 = w0 - w1 * w2 = i % 5
+    add	x21, x21, #1 // i++
+    cmp x0, #4 // i % 5 == 4
+    bge printarrafter5 // goto printarrafter5
+	b	loop2	
+printarrafter5:
+    ldr x0, =newline
+    bl printf
+    bl loop2
+endloop2:
+    ldr x0, =newline
+    bl printf
+	ldp	x29, x30, [sp], #16 // function epilog
+	ret // return
+
+        .type copy_array, @function
+// void copy_array(int dest[] - x19, int src[] - x20, int n - x21);
+copy_array:
+    stp	x29, x30, [sp, #-16]! // function prolog
+    mov	x22, #0 // i at x22
+loop3:
+    cmp	x22, x21 // i >= n
+    bge	endloop3 // goto endloop3
+    ldr w1, [x20, x22, lsl #2] // read value at base + index * 4
+    str	w1, [x19, x22, lsl #2] // store at base + index * 4
+    add	x22, x22, #1 // i++
+    b	loop3
+endloop3:
+    ldp	x29, x30, [sp], #16 // function epilog
+    ret // return
+
+		.type insertion_sort, @function
+// void insertion_sort(int arr[] - x19, int n - x20);
+insertion_sort:
+    stp	x29, x30, [sp, #-16]! // function prolog
+    mov	x22, #0 // i at x22
+loop4:
+    cmp	x22, x21 // i >= n
+    bge	endloop4 // goto endloop4
+    mov x23, x22 // j = i
+inloop4:
+	cmp x23, #0 // if j <= 0
+	ble endinloop4 // end inner loop
+	ldr w1, [x19, x23, lsl #2] // w1 = arr[j]
+	sub w2, w23, #1 // w2 = j - 1
+	ldr w3, [x19, x2, lsl #2] // w3 = arr[j - 1]
+	cmp w3, w1
+	ble endinloop4
+	str w1, [x19, x2, lsl #2] // arr[j - 1] = arr[j]
+	str w3, [x19, x23, lsl #2] // arr[j] = arr[j - 1]
+	sub w23, w23, #1
+	b inloop4
+endinloop4:
+    add	x22, x22, #1 // i++
+    b	loop4
+endloop4:
+    ldp	x29, x30, [sp], #16 // function epilog
+    ret // return
+
+.type compute_average, @function
+// int compute_average(int arr[] - x19, int n - x20);
+compute_average:
+    stp x29, x30, [sp, #-16]! // function prolog
+    mov w24, w20 // save n to w24
+    mov w21, w20 // copy n to w21
+    mov w20, #0 // initialize sum to 0
+    bl sum_array // call sum_array
+    udiv w0, w0, w24 // calculate integer average
+    ldp x29, x30, [sp], #16 // function epilog
+    ret // return
+
+
+        .type sum_array, @function
+// int sum_array(int arr[] - x19, int start - x20, int stop - x21);
+sum_array:
+	stp	x29, x30, [sp, #-16]! // function prolog
+    stp x22, x23, [sp, #-16]!  // Keep
+	cmp x21, x20
+    bgt startnoteqend
+starteqend:
+    mov x0, #0
+    b endsumarray
+startnoteqend:
+    sub w2, w21, #1 // w2 = stop - 1
+    ldr w22, [x19, x2, lsl #2] // w22 = A[stop - 1] EDIT THIS
+    sub w21, w21, #1
+    bl sum_array
+    mov w23, w0
+    add w0, w23, w22 // w0 = sum_array
+endsumarray:
+    ldp x22, x23, [sp], #16  // Keep
+	ldp	x29, x30, [sp], #16 // function epilog
+    ret // return
